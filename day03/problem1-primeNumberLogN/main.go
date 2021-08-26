017a4bb3b9005e6bb3c1e501f18bc7c1eeb99692
mozarik <mozarikfahrozi@gmail.com>
2021-08-27 00:22:30 +0700

    I think there is some bug in the sqrt root. check day5 IsPrime function

--- day03/problem1-primeNumberLogN/main.go
+++ day03/problem1-primeNumberLogN/main.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Println(PrimeNumber(1500450271))
 	fmt.Println(PrimeNumber(1000000000))
 	fmt.Println(PrimeNumber(10000000019))
-	fmt.Println(PrimeNumber(10000000033))
+	fmt.Println(IsPrimeSqrt(4))
 }
 
 func PrimeNumber(number int) bool {
@@ -24,6 +24,15 @@ func PrimeNumber(number int) bool {
 		}
 	}
 	return true
+}
+
+func IsPrimeSqrt(value int) bool {
+	for i := 2; i <= int(math.Floor(math.Sqrt(float64(value)))); i++ {
+		if value%i == 0 {
+			return false
+		}
+	}
+	return value > 1
 }
 
 func PrimeNumberNoSquareRoot(number int) bool {
